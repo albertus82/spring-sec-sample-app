667268fa28bf0b7deee67fc159fa998b8aefad7b
Alberto <8672431+albertus82@users.noreply.github.com>
2022-12-09 14:05:26 +0100

    Update SecurityConfig.java

--- spring-security-sample-web/src/main/java/com/example/spring/web/security/SecurityConfig.java
+++ spring-security-sample-web/src/main/java/com/example/spring/web/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig implements ApplicationContextAware {
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.jee().authenticatedUserDetailsService(token -> context.getBean(UserDetailsService.class).loadUserDetails(token)) // lambda required for warm context refresh
 				.mappableAuthorities(UserRole.stream().map(UserRole::getName).collect(Collectors.toSet())).and().authorizeHttpRequests().requestMatchers("/admin/**").hasAuthority(RoleNames.ROLE_ADMIN).and().csrf().disable().logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT));
 		return http.build();
