c05d324f31234d69599d1bbd1f1931c20a3116b2
Alberto <albertus82@users.noreply.github.com>
2022-12-11 09:55:54 +0100

    Removed aspectj

--- spring-security-sample-web/src/main/java/com/example/spring/web/ServletConfig.java
+++ spring-security-sample-web/src/main/java/com/example/spring/web/ServletConfig.java
@@ -2,7 +2,6 @@ package com.example.spring.web;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @ComponentScan
 @EnableWebMvc
-@EnableAspectJAutoProxy
 public class ServletConfig implements WebMvcConfigurer {
 
 	/*
