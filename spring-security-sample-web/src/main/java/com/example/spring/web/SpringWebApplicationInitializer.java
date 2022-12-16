464bb37d83b6e541e8564afcc321a433eed79c0a
Alberto <albertus82@users.noreply.github.com>
2022-12-16 09:08:01 +0100

    Refactoring.

--- spring-security-sample-web/src/main/java/com/example/spring/web/SpringWebApplicationInitializer.java
+++ spring-security-sample-web/src/main/java/com/example/spring/web/SpringWebApplicationInitializer.java
@@ -1,11 +1,10 @@
-package com.example.spring;
+package com.example.spring.web;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.example.spring.core.RootConfig;
-import com.example.spring.web.ServletConfig;
 
-public class SpringWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class SpringWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
